d7e4f16c42bb31160623584081eb6e3730447fb4
cjmcgovern56 <cjmcgovern56@gmail.com>
2026-06-03 10:08:29 -0700

    Update and rename diamond_script.sh to diamond_scfXX_script.sh

--- diamond_scfXX_script.sh
+++ diamond_scfXX_script.sh
@@ -1,5 +1,5 @@
 !/bin/bash -l
-#SBATCH --job-name=diamond_relax
+#SBATCH --job-name=Diamond_scfXX_relax !XX to range for quantaties of: 40, 60, 80, 100
 #SBATCH --partition=ckpt
 #SBATCH --account=stf-ckpt
 #SBATCH --nodes=1
@@ -14,4 +14,4 @@ module load chem/qe/6.8
 rm -rf diamond.save
 
 # Run with explicit input flag and error redirection
-mpirun -n 16 pw.x -in diamond.relax.in > diamond.relax.out 2>&1
+mpirun -n 16 pw.x -in diamond_scfXX.relax.in > diamond_scfXX.relax.out 2>&1
